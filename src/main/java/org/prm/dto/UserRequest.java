7fea8ae5354229bcf1056f8f800796e6299bfd3d
Pritiranjan Mohanty <pritiranjan.mohanty2003@gmail.com>
2026-04-12 11:07:45 +0530

    new dto

---
+++ src/main/java/org/prm/dto/UserRequest.java
@@ -0,0 +1,17 @@
+package org.prm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String username;
+    private String password;
+    private String email;
+
+}
